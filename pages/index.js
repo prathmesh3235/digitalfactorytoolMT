8ca9bdeec5f70561ff643054b66ebcca9e0df9aa
Prathmesh Doddanawar <37949352+prathmesh3235@users.noreply.github.com>
2024-06-01 17:22:40 +0200

    Flow Implementation till 3rd layer

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import "../src/app/globals.css"
 import PhasesComponent from "./phasesComponent"
 export default function Home() {
   return (
+    <div> 
     <div className="mt-2">
+    <div className="text-4xl font-bold text-blue-800 my-6 text-center">
+       The Digital Factory Planning Tool
+      </div>
     <PhasesComponent/>
+    </div>
     </div>
   );
 }
